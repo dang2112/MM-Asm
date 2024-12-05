c8c6ed90c5610534c72e2a0972f097872da63331
Dũng Nguyễn <dung.nguyenbk2310561@hcmut.edu.vn>
2024-12-05 23:28:56 +0700

    Bestfit - 1
    Fill ratio: 0.24
    Mô tả: Đây là greedy của thầy nhưng trước khi tạo mẫu cắt thì sắp xếp các chi tiết theo diện tích từ lớn đến nhỏ
    Lưu ý: Dũng đã lỡ sửa tên hàm thành 2310561 thay vì để yên như mặc định. Ae muốn test thì về

---
+++ policy2310561.py
@@ -0,0 +1,69 @@
+from policy import Policy
+
+class ProdObj:
+    size = [0,0]    # list of 2
+    demand = 0      # int
+    def __init__(self, insize, indemand):
+        self.size = insize
+        self.demand = indemand
+    def __init__(self, indict): # indict is dict
+        self.size = indict["size"]
+        self.demand = indict["quantity"]
+    def __lt__(self, other):
+        return (self.size[0] * self.size[1] < other.size[0] * other.size[1])
+
+class Policy2310561(Policy):
+    def __init__(self):
+        # Student code here
+        pass
+
+    def get_action(self, observation, info):
+        # Student code here
+        aux_list = observation["products"] # tuple of dicts
+
+        # Sort the prods in ascending
+        list_prods = list() # list of list_objs
+        for prod in aux_list:
+            # prod is tuple
+            prod_obj = ProdObj(prod)
+            list_prods.append(prod_obj)
+        list_prods.sort(reverse=True)
+
+        prod_size = [0, 0]
+        stock_idx = -1
+        pos_x, pos_y = 0, 0
+
+        # Pick a product that has quality > 0
+        for prod in list_prods:
+            if prod.demand > 0:
+                prod_size = prod.size
+
+                # Loop through all stocks
+                for i, stock in enumerate(observation["stocks"]):
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    prod_w, prod_h = prod_size
+
+                    if stock_w < prod_w or stock_h < prod_h:
+                        continue
+
+                    pos_x, pos_y = None, None
+                    for x in range(stock_w - prod_w + 1):
+                        for y in range(stock_h - prod_h + 1):
+                            if self._can_place_(stock, (x, y), prod_size):
+                                pos_x, pos_y = x, y
+                                break
+                        if pos_x is not None and pos_y is not None:
+                            break
+
+                    if pos_x is not None and pos_y is not None:
+                        stock_idx = i
+                        break
+
+                if pos_x is not None and pos_y is not None:
+                    break
+
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
+
+
+    # Student code here
+    # You can add more functions if needed
