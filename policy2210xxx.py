4591444d0953244a7aa78045e96cb59b8b2e564e
Dũng Nguyễn <dung.nguyenbk2310561@hcmut.edu.vn>
2024-12-07 22:19:18 +0700

    Implement StockObj class, not tested

--- policy2210xxx.py
+++ policy2210xxx.py
@@ -1,16 +1,26 @@
 from policy import Policy
+import numpy as np
 
 class ProdObj:
-    size = [0,0]    # list of 2
-    demand = 0      # int
     def __init__(self, insize, indemand):
-        self.size = insize
-        self.demand = indemand
+        self.size = insize      # list of 2 int
+        self.demand = indemand  # int
     def __init__(self, indict): # indict is dict
         self.size = indict["size"]
         self.demand = indict["quantity"]
     def __lt__(self, other):
         return (self.size[0] * self.size[1] < other.size[0] * other.size[1])
+
+class StockObj:
+    def __init__(self, instock, original_index): # instock is numpy array
+        self.arr = instock
+        self.height = np.sum(np.any(instock != -2, axis=0))
+        self.width = np.sum(np.any(instock != -2, axis=1))
+        self.rmarea = instock.size
+        self.used = False
+        self.orgidx = original_index
+    def __lt__(self, other):
+        return (self.rmarea < other.rmarea)
 
 class Policy2210xxx(Policy):
     def __init__(self):
@@ -19,22 +29,71 @@ class Policy2210xxx(Policy):
 
     def get_action(self, observation, info):
         # Student code here
-        aux_list = observation["products"] # tuple of dicts
 
-        # Sort the prods in ascending
+        # PREPARING DATA
+
+        # Preparing prods
+
         list_prods = list() # list of list_objs
-        for prod in aux_list:
-            # prod is tuple
+        for prod in observation["products"]:    # for (dict) in (tuple of dicts)
             prod_obj = ProdObj(prod)
             list_prods.append(prod_obj)
-        list_prods.sort(reverse=True)
+        list_prods.sort(reverse=True)   # sort the prods in descending order
 
         prod_size = [0, 0]
         stock_idx = -1
         pos_x, pos_y = 0, 0
 
-        # Pick a product that has quality > 0
+        # Preparing stocks
+        used_stocks = list()    # used stocks are kept in ascending order
+        unused_stocks = list()  # while unused stocks are kept in descending order 
+        for i, stock in enumerate(observation["stocks"]):
+            stock_obj = StockObj(stock, i)
+            unused_stocks.append(stock_obj)
+        unused_stocks.sort(reverse = True) # sort unused stocks in descending order
+
+
+
+        # ACTUAL PLACING PRODUCTS INTO STOCKS
+
+        # Ensure product has demand > 0
         for prod in list_prods:
+            if prod.demand > 0:
+                prod_size = prod.size
+                # Iterate through used stocks
+                for stock in used_stocks:
+                    stock_w = stock.weight
+                    stock_h = stock.height
+                    prod_w, prod_h = prod.size
+                    if stock_w >= prod_w and stock_h >= prod_h:
+                        pos_x, pos_y = None, None
+                        for x in range(stock_w - prod_w + 1):
+                            for y in range(stock_h - prod_h + 1):
+                                if self._can_place_(stock.arr, (x, y), prod_size):
+                                    pos_x, pos_y = x, y
+                                    break
+                            if pos_x is not None and pos_y is not None:
+                                break
+                        if pos_x is None and pos_y is None:
+                            for x in range(stock_w - prod_h + 1):
+                                for y in range(stock_h - prod_w + 1):
+                                    if self._can_place_(stock, (x, y), prod_size[::-1]):
+                                        prod_size = prod_size[::-1]
+                                        pos_x, pos_y = x, y
+                                        break
+                                if pos_x is not None and pos_y is not None:
+                                    break
+                        if pos_x is not None and pos_y is not None:
+                            stock_idx = stock.orgidx
+                            break
+                if pos_x is not None and pos_y is not None:
+                    break
+
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
+
+        # ===== OLD CODE =====================================
+        # Pick a product that has quality > 0
+        '''for prod in list_prods:
             if prod.demand > 0:
                 prod_size = prod.size
 
@@ -67,7 +126,7 @@ class Policy2210xxx(Policy):
                 if pos_x is not None and pos_y is not None:
                     break
 
-        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}'''
 
 
     # Student code here
